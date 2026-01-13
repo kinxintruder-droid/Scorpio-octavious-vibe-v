c94312393f3d77edcb1fddc98a8f2df1b9dd6567
kinxintruder-droid <kinxintruder@gmail.com>
2026-01-13 13:18:06 +0200

    Add files via upload

--- profile.js
+++ profile.js
@@ -1,16 +1,43 @@
-import { auth, db } from "./firebase.js";
-import { doc, updateDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { doc, updateDoc, arrayUnion, arrayRemove } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-window.changeNickname = async () => {
+window.followUser = async (otherUid) => {
   const user = auth.currentUser;
   if (!user) return alert("Login first");
+  if (user.uid === otherUid) return alert("Cannot follow yourself");
 
-  const newNickname = document.getElementById("nicknameInput").value.trim();
-  if (!newNickname) return alert("Enter a nickname");
-
-  await updateDoc(doc(db, "users", user.uid), {
-    nickname: newNickname
+  await updateDoc(doc(db, "following", user.uid), {
+    follows: arrayUnion(otherUid)
+  });
+  await updateDoc(doc(db, "followers", otherUid), {
+    followers: arrayUnion(user.uid)
   });
 
-  alert("Nickname updated!");
+  alert("You are now following this user!");
+};
+
+window.sendFriendRequest = async (otherUid) => {
+  const user = auth.currentUser;
+  if (!user) return alert("Login first");
+  if (user.uid === otherUid) return alert("Cannot friend yourself");
+
+  await updateDoc(doc(db, "friendRequests", otherUid), {
+    requests: arrayUnion(user.uid)
+  });
+
+  alert("Friend request sent!");
+};
+
+window.acceptFriendRequest = async (otherUid) => {
+  const user = auth.currentUser;
+  await updateDoc(doc(db, "friends", user.uid), {
+    friends: arrayUnion(otherUid)
+  });
+  await updateDoc(doc(db, "friends", otherUid), {
+    friends: arrayUnion(user.uid)
+  });
+  await updateDoc(doc(db, "friendRequests", user.uid), {
+    requests: arrayRemove(otherUid)
+  });
+
+  alert("Friend request accepted!");
 };
