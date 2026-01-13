15e77a14454d7fd6cdfbb891aef871a5e49df329
kinxintruder-droid <kinxintruder@gmail.com>
2026-01-13 17:46:56 +0200

    Add files via upload

--- firebase rules.js
+++ firebase rules.js
@@ -1,10 +1,21 @@
 rules_version = '2';
 service cloud.firestore {
-  match /databases/{database}/documents {
+  match /databases/{db}/documents {
 
-    match /messages/{convo}/{chat}/{msg} {
-      allow read, write: if request.auth != null;
+    match /wallets/{uid} {
+      allow read, write: if request.auth != null && request.auth.uid == uid;
     }
 
+    match /likes/{post}/{uid} {
+      allow write: if request.auth.uid == uid;
+    }
+
+    match /views/{post}/{uid} {
+      allow write: if request.auth.uid == uid;
+    }
+
+    match /withdrawals/{id} {
+      allow create: if request.auth != null;
+    }
   }
 }
