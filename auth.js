25532fafa9c2aafc79bcec6d2f997c4209bcb2f3
kinxintruder-droid <kinxintruder@gmail.com>
2026-01-13 13:20:16 +0200

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,4 +1,11 @@
-if (!user.emailVerified) {
-  alert("Verify your email to continue");
-  auth.signOut();
-}
+import { auth } from "./firebase.js";
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+window.login = async function () {
+  const email = email.value;
+  const password = password.value;
+  await signInWithEmailAndPassword(auth, email, password);
+  window.location = "home.html";
+};
