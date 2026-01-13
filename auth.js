1041bffe75ac7ea40340c8f1eac61d8b672438d4
kinxintruder-droid <kinxintruder@gmail.com>
2026-01-13 12:48:08 +0200

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,4 +1,12 @@
-if (!user.emailVerified) {
-  alert("Verify your email to continue");
-  auth.signOut();
-}
+// Check if URL has ?ref=REFERRER_UID
+const params = new URLSearchParams(window.location.search);
+const referrerUid = params.get("ref");
+
+auth.onAuthStateChanged(async (user) => {
+  if (user && referrerUid) {
+    await setDoc(doc(db, "referrals", user.uid), {
+      referrerUid,
+      timestamp: Date.now()
+    });
+  }
+});
