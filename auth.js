1bb343b44742225a8d98e77964e438e917139f80
kinxintruder-droid <kinxintruder@gmail.com>
2026-01-16 14:06:42 +0200

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,18 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+document.getElementById("loginBtn").addEventListener("click", login);
+
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "dashboard.html";
+    })
+    .catch(error => {
+      alert(error.message);
+    });
+}
